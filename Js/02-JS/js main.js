d0f3744a01a5532382d2f81d9a3925d18805c244
VivianZzz <ucker703@gmail.com>
2022-10-07 21:11:43 -0300

    subir

---
+++ Js/02-JS/js main.js
@@ -0,0 +1,35 @@
+var A = "Collin"
+
+var B = "disse que viria"
+
+var C = "Sexta-Feira"
+
+console.log(A + B + C)
+
+/* Collin faltou na Sexta-Feira*/
+
+
+var A = "Podemos já vislumbrar o modo pelo qual a execução dos pontos do \nprograma causa impacto indireto na reavaliação dos paradigmas corporativos."
+
+/* \n é igual a tag <br> */
+
+console.log(A)
+
+
+var A = 10 ;
+var B = "5";
+
+console.log(A + B)
+
+var A = 10 ;
+var B = "five"
+
+
+console.log(A * B)
+
+var heloisa = 10;
+var marcelo = 15;
+
+console.log(heloisa + marcelo)
+
+
