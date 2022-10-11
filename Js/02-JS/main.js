a78a6328def229515acb76736014e1cfca533e9b
VivianZzz <ucker703@gmail.com>
2022-10-10 21:03:21 -0300

    subir

--- Js/02-JS/main.js
+++ Js/02-JS/main.js
@@ -1,11 +1,14 @@
 
-var nota1= 8;
-var nota2= 7;
+var nota1= 5;
+var nota2= 4;
 var nota3= 5;
-var nota4= 6;
-var media= 8;
+var nota4= 10;
 
 
-console.log("a media do aluno com 4 notas é" + (nota1+nota2+nota3+nota4) / (media))
+var media = (nota1+nota2+nota3+nota4)/4;
+
+
+
+console.log(`A média do aluno com as 4 notas é: ${media}`)
 
 
