baa430d9fbfc14cf8ad69028a2c99ee2c316f373
VivianZzz <ucker703@gmail.com>
2022-10-10 21:42:30 -0300

    subir

---
+++ Js/03-Js/main.js
@@ -0,0 +1,18 @@
+var nota = 5;
+console.clear();
+
+console.log(" Gabriela sem camisa " + " Mayara sem camisa " + " Alunos sem Cracha ")
+console.log('A media dos alunos é ${media}')
+/*
+H = 20
+J = 5
+
+console.log(H+J)
+*/
+
+//  Acessando um carater de uma string
+
+var string1= "Vivian Maria de Oliveira e Silva"
+
+console.log(string1[0])
+console.log(string1.charAt(3))
