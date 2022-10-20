5d4529b1fa0ca98b51f6cc403b11d7ff1cdbc716
VivianZzz <ucker703@gmail.com>
2022-10-20 20:16:05 -0300

    subir

--- Js/07-Js/main.js
+++ Js/07-Js/main.js
@@ -1,6 +1,42 @@
 //1° Exercício
 
-var aluno = "gabriel"
-var nota1= ""
+var nome = "Gabriel "
+var nota1 = 10
+var nota2 = 9
+var nota3 = 8
+var nota4 = 9
+var mediaNotas = (nota1 + nota2 + nota3 + nota4) / 4
+console.log (mediaNotas)
+if (mediaNotas >= 7)
+console.log ("Aprovado")
+else
+console.log ("Reprovado")
+console.log (nome)
 
 
+var nome = "Maycon"
+var nota1 = 6
+var nota2 = 5
+var nota3 = 4
+var nota4 = 2
+var mediaNotas = (nota1 + nota2 + nota3 + nota4) / 4
+console.log (mediaNotas)
+if (mediaNotas >= 7)
+console.log ("Aprovado")
+else
+console.log ("Reprovado")
+console.log (nome)
+
+
+var nome = "Vivian"
+var nota1 = 10
+var nota2 = 10
+var nota3 = 7
+var nota4 = 9
+var mediaNotas = (nota1 + nota2 + nota3 + nota4) / 4
+console.log (mediaNotas)
+if (mediaNotas >= 7)
+console.log ("Aprovado")
+else
+console.log ("Reprovado")
+console.log (nome)
